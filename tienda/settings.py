43a9e12e88588e8de11e6f3626925857927ca7e6
Omaro Alexis Aburto Leiva <oaburto@alumnos.ubiobio.cl>
2022-02-19 20:59:36 -0300

    proyecto tienda

--- tienda/settings.py
+++ tienda/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jv8hmxi%6k9gq+i%va@)y2$qr7w_u%x=*e&7e30bh2)bvd+=s7'
+SECRET_KEY = 'django-insecure-amzv9i*^#&7c8=g7yk$=qegsul+efbv0hn%*^n$4a_#=li2hs+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
